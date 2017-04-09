a087d246ed9bfd8d2f5c0a8503712bba7abe0056
Arseny Zorin <zorinarseny@yandex.ru>
2017-04-09 18:43:25 +0300

    Adding new enums

---
+++ src/src/softwarearchs/enums/RepairType.java
@@ -0,0 +1,9 @@
+package softwarearchs.enums;
+
+/**
+ * Created by arseny on 07.04.17.
+ */
+public enum RepairType {
+    Warranty,       // Гарантийный ремонт
+    Not_warranty     // Не гарантийный ремонт
+}
