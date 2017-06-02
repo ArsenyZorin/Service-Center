9f7807ac0ace7088121ef4c9cb84aa404fa1c23e
Arseny Zorin <zorinarseny@yandex.ru>
2017-06-02 13:36:29 +0300

    GUI realization

--- src/src/softwarearchs/gui/Login.java
+++ src/src/softwarearchs/gui/Login.java
@@ -1,8 +1,18 @@
 package softwarearchs.gui;
 
+import softwarearchs.Main;
+import softwarearchs.facade.Facade;
+import softwarearchs.user.User;
 import sun.rmi.runtime.Log;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by zorin on 27.05.2017.
@@ -13,8 +23,50 @@ public class Login extends JFrame{
     private JPasswordField userPassword;
     private JTextField userLogin;
 
-    public Login(){
+    private Facade facade = Main.facade;
+    public Login() {
         setContentPane(rootPanel);
+        Dimension size = new Dimension(300, 150);
+        setSize(size);
+        setMaximumSize(size);
+        setMinimumSize(size);
+        setLocationRelativeTo(null);
+        setHandler();
         setVisible(true);
+    }
+
+    public void setHandler(){
+        Login thisFrame = this;
+        signInButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                String login = userLogin.getText();
+                String pwd = userPassword.getText();
+                if(login.isEmpty() || pwd.isEmpty()){
+                    JOptionPane.showMessageDialog(new JFrame(),
+                            "Enter login and password", "error",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                try{
+                    if(facade.signIn(login, pwd)){
+                        User currentUser = facade.getUser(login);
+                        Main.closeFrame(thisFrame);
+                        Main.showReceiptForm(currentUser);
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(new JFrame(),
+                                "Invalid login or password", "Error",
+                                JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+
+                }
+                catch (Exception e){
+                    JOptionPane.showMessageDialog(new JFrame(),
+                            e.getClass(), "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }});
     }
 }
--- src/src/softwarearchs/gui/ReceiptForm.java
+++ src/src/softwarearchs/gui/ReceiptForm.java
@@ -1,6 +1,23 @@
 package softwarearchs.gui;
 
+import org.jdatepicker.JDatePanel;
+import softwarearchs.Main;
+import softwarearchs.enums.ReceiptStatus;
+import softwarearchs.enums.RepairType;
+import softwarearchs.facade.Facade;
+import softwarearchs.receipt.Receipt;
+import softwarearchs.user.Client;
+import softwarearchs.user.User;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
 
 /**
  * Created by zorin on 27.05.2017.
@@ -34,8 +51,122 @@ public class ReceiptForm extends JFrame {
     private JButton newButton;
     private JButton UserInfoButton;
 
-    public ReceiptForm(){
+    private User currentUser;
+    private Facade facade = Main.facade;
+
+    public ReceiptForm(User user){
         setContentPane(rootPanel);
+        setLocationRelativeTo(null);
+        Dimension size = new Dimension(820, 470);
+        setSize(size);
+        setMaximumSize(size);
+        setMinimumSize(size);
+        //clearElements();
         setVisible(true);
+
+        setupHandlers();
+        receipts.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+
+        if(repairType.getItemCount() == 0)
+            for (RepairType type : RepairType.values())
+                repairType.addItem(type);
+
+        if(receiptStatus.getItemCount() == 0)
+            for(ReceiptStatus status : ReceiptStatus.values())
+                receiptStatus.addItem(status);
+
+        currentUser = user;
+        if("Receiver".equals(currentUser.getClass().getName())){
+            newButton.setEnabled(true);
+        }
     }
+
+    public void masterPermissions(){
+        repairType.setEditable(true);
+        receiptStatus.setEditable(true);
+        master.setEditable(master.getText().isEmpty());
+    }
+
+    public void receiverPermissions(){
+        repairType.setEditable(true);
+        deviceSerial.setEditable(true);
+        deviceType.setEditable(true);
+        deviceBrand.setEditable(true);
+        deviceModel.setEditable(true);
+        devicePurchaseDate.setEditable(true);
+        deviceWarrantyExpiration.setEditable(true);
+        devicePreviousRepair.setEditable(true);
+        deviceRepairWarrantyExpiration.setEditable(true);
+        clientName.setEditable(true);
+        clientSurname.setEditable(true);
+        clientPatronymic.setEditable(true);
+        clientPhoneNumber.setEditable(true);
+        clientEmail.setEditable(true);
+        deviceMalfunction.setEnabled(true);
+        deviceNote.setEnabled(true);
+        receiptStatus.setEnabled(true);
+    }
+
+    public void clearElements(){
+        SimpleDateFormat dt = new SimpleDateFormat("yyddMM");
+        SimpleDateFormat dt1 = new SimpleDateFormat("hhmmss");
+        Date today = new Date();
+        receiptNumber.setText(dt.format(today) + dt1.format(today) + currentUser.getId());
+        receiptDate.setText(today.toString());
+        repairType.setSelectedIndex(-1);
+        deviceSerial.setText("");
+        deviceType.setText("");
+        deviceBrand.setText("");
+        deviceModel.setText("");
+        devicePurchaseDate.setText("");
+        deviceWarrantyExpiration.setText("");
+        devicePreviousRepair.setText("");
+        deviceRepairWarrantyExpiration.setText("");
+        clientName.setText("");
+        clientSurname.setText("");
+        clientPatronymic.setText("");
+        clientPhoneNumber.setText("");
+        clientEmail.setText("");
+        deviceMalfunction.setText("");
+        deviceNote.setText("");
+        receiptStatus.setSelectedIndex(-1);
+        master.setText("");
+        receiver.setText(currentUser.getName() + " " + currentUser.getSurname() + " " + currentUser.getPatronymic());
+    }
+
+    public boolean receiptAdded(){
+        int receiptNumberText = Integer.parseInt(receiptNumber.getText());
+        String receiptDateDate = receiptDate.getText();
+        return false;
+    }
+
+    public void setupHandlers(){
+        ReceiptForm thisFrame = this;
+        exitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                Main.showSignIn();
+                Main.closeFrame(thisFrame);
+            }});
+
+        newButton.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent ev) {
+                if("New".equals(newButton.getText())) {
+                    clearElements();
+                    receiverPermissions();
+                    newButton.setText("Add receipt");
+                }
+                if("Add receipt".equals(newButton.getText())){
+
+                }
+
+            }});
+    }
+
+    public void masterSingedIn(){}
+
+    public void clientSignedIn(){
+        List<Receipt> clientReceipt = facade.getByClient((Client) currentUser);
+    }
+
+    public void receiverSignedIn(){}
 }
