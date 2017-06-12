6f0a3b2c8c7963a295d8c3a2082b9bfb8034dab0
Arseny Zorin <zorinarseny@yandex.ru>
2017-06-12 23:35:32 +0300

    New email exception

---
+++ src/src/softwarearchs/exceptions/EmailSendingFailed.java
@@ -0,0 +1,8 @@
+package softwarearchs.exceptions;
+
+/**
+ * Created by arseny on 12/06/17.
+ */
+public class EmailSendingFailed extends Exception{
+    public EmailSendingFailed(String message) {super(message);}
+}
