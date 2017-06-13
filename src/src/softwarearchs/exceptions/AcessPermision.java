7fca379bdf5b015d6a47122d8739896c13349c6f
Arseny Zorin <zorinarseny@yandex.ru>
2017-06-13 21:01:13 +0300

    Access exception

---
+++ src/src/softwarearchs/exceptions/AcessPermision.java
@@ -0,0 +1,8 @@
+package softwarearchs.exceptions;
+
+/**
+ * Created by arseny on 13.06.17.
+ */
+public class AcessPermision extends Exception {
+    public AcessPermision(String message) { super(message); }
+}
