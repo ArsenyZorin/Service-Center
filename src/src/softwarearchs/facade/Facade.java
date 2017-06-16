22099714395bf20415e6a60f9b0e58d741355c40
Arseny Zorin <zorinarseny@yandex.ru>
2017-06-16 21:03:58 +0300

    Facade pattern modifications

--- src/src/softwarearchs/facade/Facade.java
+++ src/src/softwarearchs/facade/Facade.java
@@ -1,11 +1,8 @@
 package softwarearchs.facade;
 
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import softwarearchs.Main;
 import softwarearchs.repair.Device;
 import softwarearchs.enums.InvoiceStatus;
-import softwarearchs.enums.ReceiptStatus;
-import softwarearchs.enums.RepairType;
 import softwarearchs.enums.Role;
 import softwarearchs.exceptions.*;
 import softwarearchs.additional.BankAccount;
@@ -21,8 +18,6 @@ import javax.mail.internet.AddressException;
 import java.util.AbstractMap;
 import java.util.Date;
 
-import static sun.audio.AudioDevice.device;
-
 /**
  * Created by zorin on 27.05.2017.
  */
@@ -33,8 +28,8 @@ public class Facade {
         repos = new MapperRepository();
     }
 
-    public boolean signIn(String login, String pwd){
-        return repos.signIn(login, pwd);
+    public User signIn(String login, String pwd) throws InvalidSignIn{
+        return User.signIn(login, pwd);
     }
 
     //User info
@@ -45,13 +40,18 @@ public class Facade {
     public User addUser(String userRole, String userName, String userSurname,
                         String userPatronymic, String userLogin,
                         String userPhoneNumber, String userEmail,
-                        String pwd) throws CreationFailed, AddressException, EmailSendingFailed {
+                        String pwd, String repeatPwd)
+            throws CreationFailed, AddressException, EmailSendingFailed, InvalidUser {
 
-
-        if(!repos.addUser(userName, userSurname, userPatronymic, userPhoneNumber,
-                userEmail, userLogin, Role.valueOf(userRole), pwd))
+        if(!pwd.equals(repeatPwd))
+            throw new CreationFailed("Different passwords");
+        User user = ((Receiver)Main.currentUser).addUser(Role.valueOf(userRole), userName, userSurname,
+                  userPatronymic, userLogin,
+                  userPhoneNumber, userEmail);
+        if(!repos.addUser(user, pwd))
             throw new CreationFailed("User creation failed");
-        User user = getUser(userLogin);
+
+        user = getUser(userLogin);
         if(!user.registrationNotification(pwd))
             throw new EmailSendingFailed("Failed to send an email");
         return user;
@@ -62,19 +62,20 @@ public class Facade {
     public boolean deleteUser(String login) { return repos.deleteUser(login); }
 
     //Device info
-    public Device addDevice(Device device) throws CreationFailed{
+    private Device addDevice(Device device) throws CreationFailed, IllegalWarranty{
+        device.isWarrantyValid();
         if(!repos.addDevice(device))
             throw new CreationFailed("Device creation failed");
         return device;
     }
     public Device getDevice(String serialNumber){ return repos.findDevice(serialNumber.toUpperCase()); }
 
-    public boolean updateDevice(Device device){ return repos.updateDevice(device); }
+    private boolean updateDevice(Device device){ return repos.updateDevice(device); }
 
     public Device getReceiptDevice(String serial, String type, String brand, String model,
                       String clientFIO, String purchaseDate, String warrantyExp,
                       String prevRepair, String repWarranty)
-            throws InvalidUser, CreationFailed, UpdationFailed {
+            throws InvalidUser, CreationFailed, UpdationFailed, IllegalWarranty {
         String clientArr[] = clientFIO.split(" ");
         Client client = (Client)getUser(clientArr[0], clientArr[1], clientArr[2]);
 
@@ -99,7 +100,7 @@ public class Facade {
 
         if(!absEqual)
             if(!updateDevice(secondDevice))
-                throw new UpdationFailed("Device updation failed");
+                throw new UpdationFailed("Device update failed");
 
         return secondDevice;
     }
@@ -108,10 +109,17 @@ public class Facade {
     public Receipt addReceipt(String receiptNumber, String receiptDate, String repairType,
                               Device device, String deviceMalfunction, String deviceNote,
                               String receiptStatus) throws InvalidUser, CreationFailed,
-            AddressException, EmailSendingFailed{
+            AddressException, EmailSendingFailed, IllegalWarranty {
+        Receipt receipt;
+        try {
+            receipt = ((Receiver) Main.currentUser).createReceipt(receiptNumber, receiptDate,
+                    repairType, device, deviceMalfunction, deviceNote, receiptStatus);
+        }catch (Exception e){
+            throw new InvalidUser("Invalid user trying to create new receipt");
+        }
 
-        Receipt receipt = ((Receiver)Main.currentUser).createReceipt(receiptNumber, receiptDate,
-                repairType, device, deviceMalfunction, deviceNote, receiptStatus);
+        receipt.isWarrantyValid();
+
         if(!repos.addReceipt(receipt))
             throw new CreationFailed("Receipt creation failed");
 
@@ -120,8 +128,12 @@ public class Facade {
 
         return receipt;
     }
-    public Receipt getReceipt(String receiptNumber){
-        return repos.findReceipt(receiptNumber);
+    public Receipt getReceipt(String receiptNumber) throws ElementNotFound {
+        Receipt receipt = repos.findReceipt(receiptNumber);
+        if(receipt == null)
+            throw new ElementNotFound("Receipt was not found");
+
+        return receipt;
     }
 
     public void updateReceipt(Receipt receipt) throws UpdationFailed, AddressException, EmailSendingFailed {
@@ -158,7 +170,7 @@ public class Facade {
     public AbstractMap<String, Invoice> getInvoicesByUser(User user) {return repos.findInvoiceByUser(user); }
     public boolean updateInvoice(Invoice invoice) throws UpdationFailed {
         if(!repos.updateInvoice(invoice))
-            throw new UpdationFailed("Invoice updation failed");
+            throw new UpdationFailed("Invoice update failed");
 
         return true;
     }
@@ -169,8 +181,8 @@ public class Facade {
 
     public Invoice addInvoice(String currentDate, Receipt receipt, String price)
             throws CreationFailed{
-        Invoice invoice = new Invoice(Main.dateFromString(currentDate), receipt);
-        invoice.setPrice(Double.parseDouble(price));
+
+        Invoice invoice = ((Receiver)Main.currentUser).createInvoice(currentDate, receipt, price);
         if(!repos.addInvoice(invoice))
             throw new CreationFailed("Invoice creation failed");
         return invoice;
@@ -186,6 +198,9 @@ public class Facade {
         BankAccount account = new BankAccount(accountNumber,
                 (Client)getUser(client[0], client[1], client[2]), validDate,
                 Integer.parseInt(cvcValue));
+
+        account.isValid();
+
         BankAccount account2 = getAccount(accountNumber);
 
         if(!account.Eq(account2))
@@ -198,5 +213,5 @@ public class Facade {
         return true;
     }
 
-    public boolean updateAccount(BankAccount account) {return repos.updateAccount(account); }
+    public boolean updateAccount(BankAccount account) { return repos.updateAccount(account); }
 }
