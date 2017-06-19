c0c913b5bce88a2298f7202bc30f455a422c4f5f
Arseny Zorin <zorinarseny@yandex.ru>
2017-06-19 21:08:59 +0300

    Some receiver changes

--- src/src/softwarearchs/user/Receiver.java
+++ src/src/softwarearchs/user/Receiver.java
@@ -2,10 +2,7 @@ package softwarearchs.user;
 
 import softwarearchs.Main;
 import softwarearchs.enums.Role;
-import softwarearchs.exceptions.AcessPermision;
-import softwarearchs.exceptions.CreationFailed;
-import softwarearchs.exceptions.InvalidPaymentData;
-import softwarearchs.exceptions.InvalidUser;
+import softwarearchs.exceptions.*;
 import softwarearchs.repair.Device;
 import softwarearchs.enums.ReceiptStatus;
 import softwarearchs.enums.RepairType;
@@ -48,12 +45,20 @@ public class Receiver extends User{
                                 String deviceWarrantyExpiration, String devicePreviousRepair,
                                 String deviceRepairWarrantyExpiration){
 
+
         Device device = new Device(deviceSerial.toUpperCase(), deviceType,
                 deviceBrand, deviceModel, client);
         device.setDateOfPurchase(Main.dateFromString(devicePurchaseDate));
         device.setWarrantyExpiration(Main.dateFromString(deviceWarrantyExpiration));
         device.setPrevRepair(Main.dateFromString(devicePreviousRepair));
         device.setRepairWarrantyExpiration(Main.dateFromString(deviceRepairWarrantyExpiration));
+
+        return device;
+    }
+
+    public Device addDevice(Device device) throws CreationFailed, IllegalWarranty {
+        device.isWarrantyValid();
+        (new MapperRepository()).addDevice(device);
 
         return device;
     }
@@ -71,36 +76,42 @@ public class Receiver extends User{
     public User addUser(Role userRole, String name, String surname,
                         String patronymic, String login,
                         String phone, String email) throws CreationFailed, InvalidUser{
-
-        if((new MapperRepository()).findUser(login) != null)
-            throw new CreationFailed("User already exists");
-        User user;
-        switch (userRole){
-            case Receiver:
-                user = new Receiver(name, surname, patronymic, login);
-                break;
-            case Master:
-                user = new Master(name, surname, patronymic, login);
-                break;
-            case Client:
-                user = new Client(name, surname, patronymic,login);
-                break;
-            default:
-                throw new InvalidUser("Role does not specified");
+        try {
+            (new MapperRepository()).findUser(login);
+        } catch (InvalidUser e) {
+            User user;
+            switch (userRole) {
+                case Receiver:
+                    user = new Receiver(name, surname, patronymic, login);
+                    break;
+                case Master:
+                    user = new Master(name, surname, patronymic, login);
+                    break;
+                case Client:
+                    user = new Client(name, surname, patronymic, login);
+                    break;
+                default:
+                    throw new InvalidUser("Role does not specified");
+            }
+            user.setPhoneNumber(phone);
+            user.seteMail(email);
+            return user;
         }
-        user.setPhoneNumber(phone);
-        user.seteMail(email);
-        return user;
+        throw new CreationFailed("User already exists");
     }
 
     public Invoice createInvoice(String currentDate, Receipt receipt, String price)
             throws CreationFailed{
+
         double priceValue = Double.parseDouble(price);
         if(priceValue == 0 && !RepairType.Warranty.equals(receipt.getRepairType()))
             throw new CreationFailed("Repair can not be free if it is not warranty");
+        else if(RepairType.Warranty.equals(receipt.getRepairType()) && priceValue != 0)
+            throw new CreationFailed("Repair can not be with price if it is warranty");
 
         Invoice invoice = new Invoice(Main.dateFromString(currentDate), receipt);
         invoice.setPrice(priceValue);
+        (new MapperRepository()).addInvoice(invoice);
         return invoice;
     }
 }
